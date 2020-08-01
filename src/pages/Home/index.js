9faf9e35be93067624e5fb994de312f9f83b0790
anabcuelbas <abcuelbas@gmail.com>
2020-08-01 13:24:26 -0300

    Arruma Carousel e inicia página de cadastro

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,35 +10,35 @@ function Home() {
     <div style={{ background: "#141414" }}>
       <Menu />
 
-      <BannerMain
-        videoTitle={dadosIniciais.categorias[1].videos[0].titulo}
-        url={dadosIniciais.categorias[1].videos[0].url}
-        videoDescription={"Estrelas de nêutrons são uma das coisas mais extremas e violentas do universo. Núcleos atômicos gigantes, com apenas alguns quilômetros de diâmetro, mas tão massivos quanto estrelas. E eles devem sua existência à morte de algo majestoso."}
-      />
+        <BannerMain
+          videoTitle={dadosIniciais.categorias[1].videos[0].titulo}
+          url={dadosIniciais.categorias[1].videos[0].url}
+          videoDescription={"Estrelas de nêutrons são uma das coisas mais extremas e violentas do universo. Núcleos atômicos gigantes, com apenas alguns quilômetros de diâmetro, mas tão massivos quanto estrelas. E eles devem sua existência à morte de algo majestoso."}
+        />
 
-      <Carousel
-        category={dadosIniciais.categorias[0]}
-      />
+        <Carousel
+          category={dadosIniciais.categorias[0]}
+        />
 
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
+        <Carousel
+          category={dadosIniciais.categorias[1]}
+        />
 
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />      
+        <Carousel
+          category={dadosIniciais.categorias[2]}
+        />      
 
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />      
+        <Carousel
+          category={dadosIniciais.categorias[3]}
+        />      
 
-      <Carousel
-        category={dadosIniciais.categorias[4]}
-      />      
+        <Carousel
+          category={dadosIniciais.categorias[4]}
+        />      
 
-      <Carousel
-        category={dadosIniciais.categorias[5]}
-      />      
+        <Carousel
+          category={dadosIniciais.categorias[5]}
+        />      
 
       <Footer />
     </div>
--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import PageDefault from '../../../components/PageDefault'
+import { Link } from 'react-router-dom'
 
 function CadastroVideo() {
     return (
-        <div>
-            Página de cadastro de vídeo
-        </div>
+        <PageDefault>
+            <h1>Cadastro de Vídeo</h1>
+
+            <Link to="/cadastro/categoria" >
+                Cadastrar Categoria
+            </Link>
+        </PageDefault>
     )
 }
 
