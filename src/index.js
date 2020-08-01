7067b112a1a0d96b93327509a605e01d16e1dd9e
anabcuelbas <abcuelbas@gmail.com>
2020-08-01 13:44:04 -0300

    Adiciona base para cadastro de categoria

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './pages/Home';
+import CadastroCategoria from './pages/cadastro/Categoria'
 import CadastroVideo from './pages/cadastro/Video'
 import Pagina404 from './pages/Page404'
 
@@ -12,6 +13,7 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={ Home } exact />
+      <Route path="/cadastro/categoria" component={ CadastroCategoria } exact />
       <Route path="/cadastro/video" component={ CadastroVideo } exact />
       <Route component={ Pagina404 } />
     </Switch>
